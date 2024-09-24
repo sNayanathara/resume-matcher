c695b1829836293106da59950c152f5f5a71ba85
sNayanathara <sandumininayanathara1@gmail.com>
2024-09-24 16:21:49 +0300

    Add endpoint for dynamic filtering of Applicants

--- backend/src/main/java/com/coding/resume_matcher/dao/ApplicantRepository.java
+++ backend/src/main/java/com/coding/resume_matcher/dao/ApplicantRepository.java
@@ -12,5 +12,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin("http://localhost:3000")
 public interface ApplicantRepository extends JpaRepository<Applicant, Integer> {
 
-    
+    @Query("select a from Applicant a where (:location is null or a.location like %:location%) and (:yearsOfExprnce is null or a.yearsOfExprnce >=:yearsOfExprnce)")
+    Page<Applicant> findApplicantsByCriteria(@Param("location") String location,
+                                             @Param("yearsOfExprnce") int yearsOfExprnce,
+                                             Pageable pageable);
 }
